091153e124e726e229b69e75d9abf8940ec2c737
pritii <pritiky910@gmail.com>
2023-04-14 21:22:47 +0530

    intent

---
+++ app/src/main/java/com/priti/intent/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.priti.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    public Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        btn = findViewById(R.id.Button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
